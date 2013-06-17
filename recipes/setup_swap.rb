46b4850eef6bc61aff4c1a1e0d05e88e7e385c3a
ryoogata <ryo.ogata@gmail.com>
2013-06-17 22:27:51 +0900

    スワップ領域を作成し mount を実行する

---
+++ recipes/setup_swap.rb
@@ -0,0 +1,23 @@
+bash 'create swapfile' do
+        code <<-EOC
+                dd if=/dev/zero of=/swap.img bs=1M count=2408 &&
+                chmod 600 /swap.img
+                mkswap /swap.img
+        EOC
+        only_if { not node[:ec2].nil? and node[:ec2][:instance_type]== 't1.micro'}
+        creates "/swap.img"
+end
+
+
+mount '/dev/null' do
+        action :enable
+        device '/swap.img'
+        fstype 'swap'
+        only_if { not node[:ec2].nil? and node[:ec2][:instance_type]== 't1.micro'}
+end
+
+
+bash 'activate swap' do
+        code 'swapon -ae'
+        only_if "test `cat /proc/swaps | wc -l` -eq 1"
+end
