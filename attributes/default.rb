89440b34e4d0cbf07134580d2906d97ac2dbe479
ryoogata <ryo.ogata@gmail.com>
2014-01-05 22:58:22 +0900

    aws のカテゴリーを追加

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 default["s3"]["_ACCESS_KEY"] = ""
 default["s3"]["_SECRET_KEY"] = ""
+default["s3"]["_S3_BUCKETS_NAME"] = ""
 default["backup"]["_BACKUP_DIRECTORY"] = "/var, /tmp"
+default["backup"]["_BACKUPED_FILE_PREFIX"] = "filebackup"
+default["backup"]["_BACKUPED_DB_PREFIX"] = "dbbackup"
