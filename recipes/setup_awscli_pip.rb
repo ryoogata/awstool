880bcb74b9704e070a2b54c710adc3e7d118aa3c
ryoogata <ryo.ogata@gmail.com>
2014-01-20 23:20:15 +0900

    aws cli tool をインストールする Recipes を追加

---
+++ recipes/setup_awscli_pip.rb
@@ -0,0 +1,17 @@
+# python-pip のインストール
+case node['platform']
+when "centos"
+	package "python-pip" do
+		action :install
+	end
+end
+
+# pip のインストール
+easy_install_package "pip" do
+	action :install
+end
+
+# pip 経由の aws cli のインストール
+execute "pip install" do
+        command "/usr/bin/pip install awscli"
+end
---
+++ recipes/setup_awscli_source.rb
@@ -0,0 +1,18 @@
+git "/usr/local/src/aws-cli" do
+        repository "git://github.com/aws/aws-cli.git"
+        reference "master"
+        action :sync
+end
+
+package "python-setuptools" do
+  action :install
+end
+
+script "install-aws-cli" do
+  interpreter "bash"
+  user "root"
+  cwd "/usr/local/src/aws-cli"
+  code <<-EOH
+    python setup.py install
+  EOH
+end
