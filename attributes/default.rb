2bb960148b07f3af7b4376886f723db47c149bc2
ryoogata <ryo.ogata@gmail.com>
2014-01-24 22:56:10 +0900

    AWS CLI の為に Attribute を追加と不要な Attribute を削除

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,11 @@ default["aws"]["_ACCESS_KEY"] = ""
 default["aws"]["_SECRET_KEY"] = ""
 default["aws"]["dynamodb"]["_DYNAMODB_REGION"] = "us-west-1"
 default["aws"]["dynamodb"]["_TABLE_NAME"] = "AWSCPUGacha"
-default["s3"]["_ACCESS_KEY"] = ""
-default["s3"]["_SECRET_KEY"] = ""
+default["aws"]["cli"]["install_method"] = "source"
+
 default["s3"]["_S3_BUCKETS_NAME"] = ""
+
 default["backup"]["_BACKUP_DIRECTORY"] = "/var, /tmp"
 default["backup"]["_BACKUPED_FILE_PREFIX"] = "filebackup"
 default["backup"]["_BACKUPED_DB_PREFIX"] = "dbbackup"
+
