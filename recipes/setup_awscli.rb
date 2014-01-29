7690f4afc23f39da6ea068c4fe51dc4606a15f36
root <root@localhost.localdomain>
2014-01-29 22:38:37 +0900

    インストール方法をAttribute から指定するよう変更

--- recipes/setup_awscli.rb
+++ recipes/setup_awscli.rb
@@ -1,30 +1,16 @@
-# python-pip のインストール
-case node['platform']
-when "centos"
-	package "python-pip" do
-		action :install
-	end
-end
+include_recipe "awstool::setup_awscli_#{node["aws"]["cli"]['install_method']}"
 
-# pip のインストール
-easy_install_package "pip" do
-	action :install
-end
-
-# pip 経由の aws cli のインストール
-execute "pip install" do
-        command "/usr/bin/pip install awscli"
-end
 
 # 設定ファイル用の Directory を作成
 directory "/root/.aws" do
-	action :create
+  action :create
 end
+
 
 # 設定ファイルの設置
 template "/root/.aws/config" do
-        source "config.erb"
-        owner "root"
-        group "root"
-        mode 0600
+  source "config.erb"
+  owner "root"
+  group "root"
+  mode 0600
 end
