31f400327716a079a60a88453e5fba05f17cf1b8
ryoogata <ryo.ogata@gmail.com>
2014-06-13 23:34:24 +0900

    aws t1.micro のみ実行するよう変更

--- recipes/setup_cgroup.rb
+++ recipes/setup_cgroup.rb
@@ -2,6 +2,7 @@ case node['platform']
 when "centos","amazon"
   package "libcgroup" do
     action :install
+    only_if { not node[:ec2].nil? and node[:ec2][:instance_type] == 't1.micro'}
   end
 end
 
@@ -10,15 +11,18 @@ when "centos"
   template "/etc/cgconfig.conf" do
     source "cgconfig.conf.erb"
     notifies :restart, "service[cgconfig]", :immediately
+    only_if { not node[:ec2].nil? and node[:ec2][:instance_type] == 't1.micro'}
   end
 when "amazon"
   template "/etc/cgconfig.conf" do
     source "cgconfig.conf.amazon.erb"
     notifies :restart, "service[cgconfig]", :immediately
+    only_if { not node[:ec2].nil? and node[:ec2][:instance_type] == 't1.micro'}
   end
 end
 
 service "cgconfig" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
+  only_if { not node[:ec2].nil? and node[:ec2][:instance_type] == 't1.micro'}
 end
