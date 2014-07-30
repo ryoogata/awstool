223234aad7acc4fa986b17e810be97b5f95b81af
ryoogata <ryo.ogata@gmail.com>
2014-07-30 17:01:03 +0900

    t2.micro の場合も swap を作成するよう変更

--- recipes/setup_swap.rb
+++ recipes/setup_swap.rb
@@ -8,7 +8,7 @@ bash 'create swapfile' do
           chmod 600 /swap.img
           mkswap /swap.img
   EOC
-  only_if { not node[:ec2].nil? and node[:ec2][:instance_type] == 't1.micro'}
+  only_if { not node[:ec2].nil? and ( node[:ec2][:instance_type] == 't1.micro' or node[:ec2][:instance_type] == 't2.micro' ) }
   creates "/swap.img"
 end
 
@@ -16,7 +16,7 @@ mount '/dev/null' do
   action :enable
   device '/swap.img'
   fstype 'swap'
-  only_if { not node[:ec2].nil? and node[:ec2][:instance_type] == 't1.micro'}
+  only_if { not node[:ec2].nil? and ( node[:ec2][:instance_type] == 't1.micro' or node[:ec2][:instance_type] == 't2.micro' ) }
 end
 
 bash 'activate swap' do
